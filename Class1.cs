9a1aa838baa861ceae19553f54ff66501ba26eaf
ingrydasouza-hub <ingryd.a.souza@aluno.senai.br>
2026-05-07 14:29:11 -0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicita o primeiro número
+        Console.Write("Digite o primeiro número: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        // Solicita o segundo número
+        Console.Write("Digite o segundo número: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        // Calcula a soma
+        int resultado = num1 + num2;
+
+        // Exibe o resultado
+        Console.WriteLine("Resultado: " + resultado);
+    }
+}
