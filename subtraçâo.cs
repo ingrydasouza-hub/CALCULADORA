2274d7149e24a947358ee72f9b56b2d4284ff9fa
ingrydasouza-hub <ingryd.a.souza@aluno.senai.br>
2026-05-07 14:39:56 -0300

    Add files via upload

---
+++ subtraçâo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+      // Calcula a soma
+        int resultado = num1 - num2;
+
+        // Exibe o resultado
+        Console.WriteLine("subtração: " + resultado);
+    }
+}
